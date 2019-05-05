36b44c77fa3f1f2b6f47f688dceffd55c7e00797
matthew-trump <matthew.trump@gmail.com>
2019-05-04 19:11:07 -0700

    add material components and services for demo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,58 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { AngularFireModule } from '@angular/fire';
+import { AngularFireAuthModule } from '@angular/fire/auth';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+
+import { MatToolbarModule } from '@angular/material';
+import { MatMenuModule } from '@angular/material';
+import { MatGridListModule } from '@angular/material';
+import { MatIconModule } from '@angular/material';
+import { MatButtonModule } from '@angular/material';
+import { MatInputModule } from '@angular/material';
+import { MatTooltipModule } from '@angular/material';
+import { MatSelectModule } from '@angular/material';
+import { MatTableModule } from '@angular/material';
+import { MatProgressSpinnerModule } from '@angular/material';
+import { MatTabsModule } from '@angular/material';
+import { MatCardModule } from '@angular/material';
+import { MatRadioModule } from '@angular/material';
+import { MatCheckboxModule } from '@angular/material';
+import { MatDialogModule } from '@angular/material';
+
+import { environment } from 'src/environments/environment';
+import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireAuthModule,
+    AngularFirestoreModule.enablePersistence(),
+
+    MatToolbarModule,
+    MatMenuModule,
+    MatGridListModule,
+    MatIconModule,
+    MatButtonModule,
+    MatCardModule,
+    MatInputModule,
+    MatTooltipModule,
+    MatCheckboxModule,
+    MatSelectModule,
+    MatTableModule,
+    MatTabsModule,
+    MatRadioModule,
+    MatDialogModule,
+    MatProgressSpinnerModule
   ],
   providers: [],
   bootstrap: [AppComponent]
