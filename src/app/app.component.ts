374e6cc9f1c03cb58d24d0510aec781236d37267
matthew-trump <matthew.trump@gmail.com>
2019-05-04 19:11:19 -0700

    add ref to auth service and user

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Router } from '@angular/router';
+
+import { AuthService } from './auth.service';
+import { User } from './user';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'angular-firestore-auth';
+export class AppComponent implements OnInit {
+
+  title: string;
+  testMenu: boolean;
+
+  user: Observable<User>;
+
+  constructor(
+    private authService: AuthService,
+    private router: Router
+  ) {
+
+    this.user = this.authService.user;
+  }
+  ngOnInit() {
+
+  }
+  goToMenuChoice(choice) {
+    this.router.navigate(['/'].concat(choice));
+  }
 }
