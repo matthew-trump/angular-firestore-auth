e75642e4f7d2c187dc28382c48029f59eb71ee8f
matthew-trump <matthew.trump@gmail.com>
2019-05-04 19:09:36 -0700

    main module that provides routes and lazy loading for admin

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent, pathMatch: 'full' },
+  { path: 'login', component: LoginComponent },
+  { path: 'admin', loadChildren: './admin/admin.module#AdminModule' }
+]
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
